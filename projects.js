df133ff5d6205562a27cbe069f8fcf68d8e7abaa
sanjidaorpi <sanjidaorpi111@gmail.com>
2025-03-10 14:25:18 -0400

    updated project

--- projects.js
+++ projects.js
@@ -3,119 +3,175 @@ let outputGridElement;
 let projectDisplayElement;
 
 let projCollection = [
+
   {
-    "itemTitle": "Fullourish",
-    "category": "Web Dev",
+    "itemTitle": "Fantasy Sports Database",
+    "category": "Back End",
     "id": "1",
-    "image": "images/fullourish.png",
-    "language": "HTML/CSS"
+    "image": "images/fantasy.png",
+    "language": "SQL, PHP, HTML/CSS"
   },
 
   {
     "itemTitle": "Cache Simulator",
-    "category": "Data Analysis",
+    "category": "Back End",
     "id": "2",
     "image": "images/simcache.png",
     "language": "C++"
   },
+
   {
-    "itemTitle": "405 Stuck in Cyberspace",
-    "category": "Creative",
+    "itemTitle": "Fullourish",
+    "category": "Web Dev",
     "id": "3",
-    "image": "images/cyberspace.png",
-    "language": "P5JS"
+    "image": "images/fullourish.png",
+    "language": "API, JS, HTML/CSS"
   },
+
   {
-    "itemTitle": "Carrot Catcher",
+    "itemTitle": "Paw Pals",
     "category": "Game Dev",
-    "id": "7",
-    "image": "images/carrotcatcher.png",
+    "id": "4",
+    "image": "images/pawpals.png",
     "language": "GML"
   },
-  {
-    "itemTitle": "Interactive STEM Mural",
-    "category": "Creative",
-    "id": "7",
-    "image": "images/mural.jpeg",
-    "language": "C"
-  },
-  {
-    "itemTitle": "CatCam",
-    "category": "Mobile Dev",
-    "id": "4",
-    "image": "images/catcam.png",
-    "language": "Swift"
-  },
-  {
-    "itemTitle": "iAct",
-    "category": "Mobile Dev",
-    "id": "5",
-    "image": "images/iAct.png",
-    "language": "Swift"
-  },
-  {
-    "itemTitle": "TandonStudy",
-    "category": "Web Dev",
-    "id": "6",
-    "image": "images/tandonstudy.png",
-    "language": "HTML/CSS"
-  },
-  {
-    "itemTitle": "Tic-Tac-Toe Game",
-    "category": "Data Analysis",
-    "id": "7",
-    "image": "images/tic_tac_toe.png",
-    "language": "R"
-  },
-  {
-    "itemTitle": "Galton Board Simulation",
-    "category": "Data Analysis",
-    "id": "8",
-    "image": "images/galton_board.jpg",
-    "language": "R"
-  },
-  {
-    "itemTitle": "Number Line ML Model",
-    "category": "Data Analysis",
-    "id": "9",
-    "image": "images/number_line.jpg",
-    "language": "R"
-  },
+
   {
     "itemTitle": "Inverse Probability Weighting, Sensitivity Analysis",
     "category": "Data Analysis",
-    "id": "10",
+    "id": "5",
     "image": "images/sensitivity_analysis.png",
     "language": "R"
   },
+
+  {
+    "itemTitle": "Self Reflection",
+    "category": "Creative",
+    "id": "6",
+    "image": "images/selfreflection.png",
+    "language": "p5js"
+  },
+
+  {
+    "itemTitle": "405 Stuck in Cyberspace",
+    "category": "Creative",
+    "id": "7",
+    "image": "images/cyberspace.png",
+    "language": "p5js"
+  },
+
+  {
+    "itemTitle": "CatCam",
+    "category": "Mobile Dev",
+    "id": "8",
+    "image": "images/catcam.png",
+    "language": "Swift"
+  },
+
+  {
+    "itemTitle": "iAct",
+    "category": "Mobile Dev",
+    "id": "9",
+    "image": "images/iAct.png",
+    "language": "Swift"
+  },
+
+  {
+    "itemTitle": "TandonStudy",
+    "category": "Web Dev",
+    "id": "10",
+    "image": "images/tandonstudy.png",
+    "language": "HTML/CSS"
+  },
+
+  {
+    "itemTitle": "Tic-Tac-Toe Game",
+    "category": "Data Analysis",
+    "id": "11",
+    "image": "images/tic_tac_toe.png",
+    "language": "R"
+  },
+
+  {
+    "itemTitle": "Galton Board Simulation",
+    "category": "Data Analysis",
+    "id": "12",
+    "image": "images/galton_board.jpg",
+    "language": "R"
+  },
+  
+  {
+    "itemTitle": "Number Line ML Model",
+    "category": "Data Analysis",
+    "id": "13",
+    "image": "images/number_line.jpg",
+    "language": "R"
+  },
+
   {
     "itemTitle": "Audio Synthesizer and Reactive Visualizer",
     "category": "Creative",
-    "id": "11",
+    "id": "14",
     "image": "images/audio_vis.jpg",
     "language": "Max"
   },
+
   {
     "itemTitle": "Breakout Game",
     "category": "Game Dev",
-    "id": "12",
+    "id": "15",
     "image": "images/breakout.jpg",
     "language": "p5js"
   },
+
+  {
+    "itemTitle": "Fish Frenzy",
+    "category": "Game Dev",
+    "id": "16",
+    "image": "images/fishfrenzy.png",
+    "language": "p5js"
+  },
+
+  {
+    "itemTitle": "CWAZY CUPCAKES",
+    "category": "Creative",
+    "id": "17",
+    "image": "images/cupcakes.png",
+    "language": "p5js"
+  },
+
   {
     "itemTitle": "FruitDrop Game",
     "category": "Game Dev",
-    "id": "13",
+    "id": "18",
     "image": "images/fruitdrop.jpg",
     "language": "p5js"
   },
+
+  {
+    "itemTitle": "Carrot Catcher",
+    "category": "Game Dev",
+    "id": "19",
+    "image": "images/carrotcatcher.png",
+    "language": "GML"
+  },
+
   {
     "itemTitle": "Motivational Bookshelf",
     "category": "Creative",
-    "id": "14",
+    "id": "20",
     "image": "images/bookshelf.png",
     "language": "Arduino"
+  },
+
+  {
+    "itemTitle": "Interactive STEM Mural",
+    "category": "Creative",
+    "id": "21",
+    "image": "images/mural.jpeg",
+    "language": "C"
   }
+
 ];
 
 function filterSelection(category) {
