6450cc900877d94f8c6733a4ac050a95df4706e6
sanjidaorpi <sanjidaorpi111@gmail.com>
2025-05-20 11:57:26 -0400

    Mobile compatability improved

--- sketch.js
+++ sketch.js
@@ -77,21 +77,6 @@ function checkBounds() {
   }
 }
 
-function star(x, y, radius1, radius2, npoints) {
-  let angle = TWO_PI / npoints;
-  let halfAngle = angle / 2.0;
-  beginShape();
-  for (let a = 0; a < TWO_PI; a += angle) {
-    let sx = x + cos(a) * radius2;
-    let sy = y + sin(a) * radius2;
-    vertex(sx, sy);
-    sx = x + cos(a + halfAngle) * radius1;
-    sy = y + sin(a + halfAngle) * radius1;
-    vertex(sx, sy);
-  }
-  endShape(CLOSE);
-}
-
 function petal(x, y, r) {
   beginShape();
   curveVertex(x, y);
